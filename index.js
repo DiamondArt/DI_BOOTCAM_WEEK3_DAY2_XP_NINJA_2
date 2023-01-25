236271d4e3147fb894439aa15296f30cf1b027a8
Melissa Kouadio <angemelisk@gmail.com>
2023-01-25 22:54:51 +0000

    WeeK3 day2 xp-ninja-2

---
+++ index.js
@@ -0,0 +1,35 @@
+
+/***********************************************************************************
+ * ---------------------------------------------------------------------------------
+ * @author Melissa Kouadio <angemelisk@gmail.com>
+ * @description : 
+ * Create login and register form slider on same page
+ * **********************************************************************************/   
+
+const signIn = document.querySelector("#signInButton");
+const signUp = document.querySelector("#signUpButton");
+const signInForm = document.querySelector(".container .sign-in-form");
+const signUpForm = document.querySelector(".container .sign-up-form");
+const overlay_container = document.querySelector(
+  ".container .overlay-container"
+);
+const overlay = document.querySelector(
+  ".container .overlay-container .overlay"
+);
+
+// sign in click event
+signIn.addEventListener("click", () => {
+  overlay_container.style.transform = "translateX(100%)";
+  overlay.style.transform = "translateX(-50%)";
+  signInForm.classList.add("active");
+  signUpForm.classList.remove("active");
+});
+
+// sign up click event
+
+signUp.addEventListener("click", () => {
+  overlay_container.style.transform = "translateX(0)";
+  overlay.style.transform = "translateX(0)";
+  signUpForm.classList.add("active");
+  signInForm.classList.remove("active");
+});
